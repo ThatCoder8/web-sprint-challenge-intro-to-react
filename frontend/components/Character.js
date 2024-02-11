812235b969875d9b6e6469aa91f906707f0de4c6
Sara Chein <sarahchein@gmail.com>
2024-02-11 16:57:44 -0500

    solved

--- frontend/components/Character.js
+++ frontend/components/Character.js
@@ -1,17 +1,5 @@
-// import React from 'react'
-
-// function Character() { // ❗ Add the props
-//   // ❗ Create a state to hold whether the homeworld is rendering or not
-//   // ❗ Create a "toggle" click handler to show or remove the homeworld
-//   return (
-//     <div>
-//       {/* Use the same markup with the same attributes as in the mock */}
-//     </div>
-//   )
-// }
-
-// export default Character
 import React, { useState } from 'react';
+
 
 function Character({ character }) {
   const [showHomeworld, setShowHomeworld] = useState(false);
@@ -20,9 +8,12 @@ function Character({ character }) {
     setShowHomeworld(!showHomeworld);
   };
 
+ // ❗ Add the props
+  // ❗ Create a state to hold whether the homeworld is rendering or not
+  // ❗ Create a "toggle" click handler to show or remove the homeworld
   return (
-    <div className="character-card">
-      <h3 className={`character-name ${showHomeworld ? 'active' : ''}`} onClick={toggleHomeworld}>
+    <div className="character-card" onClick={toggleHomeworld}>
+      <h3 className={`character-name ${showHomeworld ? 'active' : ''}`} >
         {character.name}
       </h3>
 
